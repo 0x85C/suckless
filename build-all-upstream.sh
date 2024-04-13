329dd713a04936b4f9a6a6347816b4f885ceab0d
0x85C <21167907+0x85C@users.noreply.github.com>
2024-04-14 05:50:21 +1000

    update build script

--- build-all-upstream.sh
+++ build-all-upstream.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 # Backup all patch files
-mkdir -p ./temp-patches/dmenu
-mkdir -p ./temp-patches/st
-mkdir -p ./temp-patches/slock
+mkdir ./temp-patches
+mkdir ./temp-patches/dmenu
+mkdir ./temp-patches/st
+mkdir ./temp-patches/slock
 mv ./dmenu/patches/* ./temp-patches/dmenu/
 mv ./st/patches/* ./temp-patches/st/
 mv ./slock/patches/* ./temp-patches/slock/
@@ -16,11 +17,18 @@ git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/slock
 
+# Restore all patch files 
+mkdir ./dmenu/patches
+mv ./temp-patches/dmenu/* ./dmenu/patches
+mkdir ./st/patches
+mv ./temp-patches/st/* ./st/patches
+mkdir ./slock/patches
+mv ./temp-patches/slock/* ./slock/patches
+rm -rf ./temp-patches
+
 # Build dmenu 5.3 with patches
 cd ./dmenu
 git checkout 5.3
-mkdir ./patches
-mv ./../temp-patches/dmenu/* ./patches/
 git apply patches/dmenu-localpaths-5.3-0x85c.diff
 git apply patches/dmenu-center-5.3-0x85c.diff
 git apply patches/dmenu-border-5.3-0x85c.diff
@@ -30,8 +38,6 @@ cd ../
 # Build st 0.9.2 with patches
 cd ./st
 git checkout 0.9.2
-mkdir ./patches
-mv ./../temp-patches/st/* ./patches/
 git apply patches/st-dracula-0.9.2-0x85c.diff
 git apply patches/st-scrollback-0.9.2-0x85c.diff
 git apply patches/st-scrollback-reflow-0.9.2-0x85c.diff
@@ -42,11 +48,6 @@ cd ../
 # Build slock 1.5 with patches
 cd ./slock
 git checkout 1.5
-mkdir ./patches
-mv ./../temp-patches/slock/* ./patches/
 git apply patches/slock-all-black-1.5-0x85c.diff
 make clean all
 cd ../
-
-# clean up
-rm -rf ./temp-patches
